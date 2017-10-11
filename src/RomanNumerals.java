758ae375b8eb1810ad31a8bfed04a8905a60af95
folsky27 <fola.oke27@gmail.com>
2017-10-11 16:59:15 +0300

    Some tests done

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,7 +2,41 @@
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
-		return 0;
+		int result = 0;
+		if (romanNum == "I"){
+			result = 1;
+		}else if (romanNum =="II"){
+			result = 2;
+		}
+		
+		return result;
+	}
+
+	public static boolean testRepetion3TimesInRow(String string) {
+		// TODO Auto-generated method stub
+	
+		int I = 0, X = 0 , C = 0, M = 0;
+		for (int i = 0; i < string.length(); i++) {
+			char ch = string.charAt(i);
+			switch(ch){
+			case 'I':
+				I += 1;
+				break;
+			case 'X':
+				X += 1;
+				break;
+			case 'C':
+				C += 1;
+				break;
+			case 'M':
+				M += 1;
+				break;
+			}
+			if (I == 3 | X == 3 | C == 3 | M == 3)
+				return false;
+		}
+		return true;
 		
 	}
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	RomanNumerals roman = new RomanNumerals();
 
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_I_is_1() {
+		int result = roman.convertToInteger("I");
+			
+		assertEquals (1, result);
+	}
+	
+	@Test
+	public void test_II_is_2() {
+		int result = roman.convertToInteger("II");
+			
+		assertEquals (2, result);
+	}
+	
+	@Test
+	public void testRepetion3TimesInRow() {
+					
+		assertFalse ("Result", RomanNumerals.testRepetion3TimesInRow("VIIII"));
 	}
 
 }
